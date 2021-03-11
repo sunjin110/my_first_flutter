bfb763a50f08489fb8a199dc2c5d167e6fd15d50
yun_sunjin <yun_sunjin@cocone.co.jp>
2021-03-11 18:10:38 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main(List<String> args) {
+  // debug
+  debugPaintSizeEnabled = true;
+  debugPaintBaselinesEnabled = true;
+  debugPaintLayerBordersEnabled = true;
+  debugRepaintRainbowEnabled = true;
+
+  // main
   runApp(MyApp());
 }
 
@@ -9,15 +16,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Welcome to Flutter",
+      title: "Flutter layout demo",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to Flutter2"),
-        ),
-        body: Center(
-          child: Text("Hello World"),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("Flutter layout demo2"),
+          ),
+          body: Column(
+            children: [_titleSection, _createButtonSection(context)],
+          )),
     );
   }
+}
+
+// title section
+final Widget _titleSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: Text(
+              "Oeschinen Lake Campground",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Text(
+            "Kandersteg Switzerland",
+            style: TextStyle(color: Colors.grey[500]),
+          )
+        ],
+      )),
+      Icon(Icons.star, color: Colors.red[500]),
+      Text("41")
+    ],
+  ),
+);
+
+Widget _createButtonSection(BuildContext context) {
+  Color color = Theme.of(context).primaryColor;
+
+  return Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtionColumn(color, Icons.call, "Call"),
+        _buildButtionColumn(color, Icons.near_me, "ROUTE"),
+        _buildButtionColumn(color, Icons.share, "SHARE")
+      ],
+    ),
+  );
+}
+
+Column _buildButtionColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(
+        icon,
+        color: color,
+      ),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+              fontSize: 12, fontWeight: FontWeight.w400, color: color),
+        ),
+      )
+    ],
+  );
 }
