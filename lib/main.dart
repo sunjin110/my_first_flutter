8c5fac224a5f667aee4cb33180aa11cd75d37c40
yun_sunjin <yun_sunjin@cocone.co.jp>
2021-03-11 16:38:15 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Welcome to Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to Flutter2"),
+        ),
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
